38fcefd746642f888d67eaef2e75e60a8c913a7b
artemka5725 <tema5725@mail.ru>
2025-11-09 22:20:51 +0300

    Сверстал по быстрому калькулятор и сделал базовый функционал

---
+++ script.js
@@ -0,0 +1,84 @@
+const resultDisplay = document.querySelector('input');
+const  buttonsNumber = document.querySelectorAll('.number');
+const  clearButton = document.querySelector('.clear');
+const buttonsAction = document.querySelectorAll('.action');
+const buttonResult = document.querySelector('.equals');
+const arrayKeys =['1','2','3','4','5','6','7','8','9','0','-','=','+','*','/','.','Enter','Backspace'];
+
+
+
+clearButton.addEventListener('click', () => {
+    resultDisplay.value = '0';
+});
+
+resultDisplay.addEventListener('keydown', (e) => {
+    if (arrayKeys.includes(e.key)) {
+
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            calculateResultDisplay(resultDisplay);
+            return;
+        }
+
+        if (e.key === 'Backspace') {
+            return;
+        }
+        e.preventDefault();
+
+        if (resultDisplay.value === '0' && e.key !== '.') {
+            resultDisplay.value = e.key;
+        } else {
+            resultDisplay.value += e.key;
+        }
+    } else {
+        e.preventDefault();
+    }
+});
+
+buttonsNumber.forEach((button) => {
+    button.addEventListener('click',  (e) => {
+        if (resultDisplay.value === '0') {
+            resultDisplay.value = button.textContent;
+
+        } else {
+            resultDisplay.value += button.textContent;
+
+        }
+    });
+});
+
+buttonsAction.forEach((button) => {
+    button.addEventListener('click',  (e) => {
+            resultDisplay.value += button.textContent;
+    });
+});
+
+buttonResult.addEventListener('click', (e) => {
+    calculateResultDisplay(resultDisplay);
+});
+
+
+function normalizeExpression(expression) {
+    return expression
+        .replace(/×/g, '*')
+        .replace(/÷/g, '/')
+        .replace(/√/g, 'Math.sqrt')
+        .replace(/%/g, '/100')
+}
+
+function calculateResultDisplay(resultDisplay) {
+    const normalExpression = normalizeExpression(resultDisplay.value);
+    return resultDisplay.value = new Function('return ' + normalExpression )();
+}
+
+
+
+
+
+
+
+
+
+
+
+
