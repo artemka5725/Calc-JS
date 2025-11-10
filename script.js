d14f708e9f772e802aa6b9bd6b1923fd0065f66b
artemka5725 <aartemstepanov57@mail.ru>
2025-11-10 16:40:36 +0300

    Заменил кнопку корня на кнопку добавления минуса перед числом, так как корень будет в расширенной версии калькулятора

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const  buttonsNumber = document.querySelectorAll('.number');
 const  clearButton = document.querySelector('.clear');
 const buttonsAction = document.querySelectorAll('.action');
 const buttonResult = document.querySelector('.equals');
+const buttonPlusMinus = document.querySelector('.plus-minus');
 const arrayKeys =['1','2','3','4','5','6','7','8','9','0','-','+','*','/','.','Enter','Backspace'];
+const operators = ['+', '-', '×', '÷', '*', '/', '.','+/-','%','Math.sqrt'];
 
 
 
@@ -36,7 +38,7 @@ resultDisplay.addEventListener('keydown', (e) => {
 });
 
 buttonsNumber.forEach((button) => {
-    button.addEventListener('click',  (e) => {
+    button.addEventListener('click',  () => {
         if (resultDisplay.value === '0') {
             resultDisplay.value = button.textContent;
 
@@ -48,14 +50,29 @@ buttonsNumber.forEach((button) => {
 });
 
 buttonsAction.forEach((button) => {
-    button.addEventListener('click',  (e) => {
+    button.addEventListener('click',  () => {
+        const lastSymbolInResultDisplay = resultDisplay.value[resultDisplay.value.length - 1];
         if (lastSymbolIsOperator(resultDisplay.value)) {
+            resultDisplay.value += button.textContent;
+        } else if (operators.includes(lastSymbolInResultDisplay)) {
+            resultDisplay.value = resultDisplay.value.slice(0,-1);
             resultDisplay.value += button.textContent;
         }
     });
 });
 
-buttonResult.addEventListener('click', (e) => {
+buttonPlusMinus.addEventListener('click', () => {
+    if (resultDisplay.value === '0') {
+        resultDisplay.value = '-';
+    } else if (resultDisplay.value.startsWith('-')) {
+        resultDisplay.value = resultDisplay.value.slice(1);
+    } else {
+        resultDisplay.value = '-' + resultDisplay.value;
+    }
+})
+
+
+buttonResult.addEventListener('click', () => {
         calculateResultDisplay(resultDisplay);
         if (resultDisplay.value === 'NaN') {
             resultDisplay.value = '0';
@@ -67,25 +84,25 @@ function normalizeExpression(expression) {
     return expression
         .replace(/×/g, '*')
         .replace(/÷/g, '/')
-        .replace(/√/g, 'Math.sqrt')
-        .replace(/%/g, '/100')
+        .replace(/%/g, '/100');
 }
+    function calculateResultDisplay(resultDisplay) {
+        const normalExpression = normalizeExpression(resultDisplay.value);
+        return resultDisplay.value = new Function('return ' + normalExpression)();
 
-function calculateResultDisplay(resultDisplay) {
-    const normalExpression = normalizeExpression(resultDisplay.value);
-    return resultDisplay.value = new Function('return ' + normalExpression )();
-}
-
-function lastSymbolIsOperator (expression) {
-
-    if (expression.length === 0) {
-        return false;
     }
-    const lastSymbol = expression[expression.length - 1];
-    const operators = ['+', '-', '×', '÷', '*', '/', '.'];
 
-    return !operators.includes(lastSymbol);
-}
+    function lastSymbolIsOperator(expression) {
+
+        if (expression.length === 0) {
+            return false;
+        }
+        const lastSymbol = expression[expression.length - 1];
+
+
+        return !operators.includes(lastSymbol);
+    }
+
 
 
 
